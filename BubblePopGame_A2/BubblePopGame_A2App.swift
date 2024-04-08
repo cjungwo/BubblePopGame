6e486671b0e5c72eabe9dc4574d56d63a3dffae3
Cjungwo <jwoo516@naver.com>
2024-04-08 11:29:31 +1000

    Initial Commit

---
+++ BubblePopGame_A2/BubblePopGame_A2App.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopGame_A2App.swift
+//  BubblePopGame_A2
+//
+//  Created by JungWoo Choi on 8/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct BubblePopGame_A2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubblePopGame_A2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BubblePopGame_A2
+//
+//  Created by JungWoo Choi on 8/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
